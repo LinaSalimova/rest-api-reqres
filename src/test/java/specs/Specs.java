0d6a0c85c95dbf2da16c52fd5fa971c66c24c585
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-06-23 14:19:21 +0300

    + changes

--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -9,62 +9,42 @@ import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class Specs {
-        public static RequestSpecification loginRequestSpecBase = with()
-                .log().uri()
-                .log().body()
-                .filter(withCustomTemplates())
-                .contentType(JSON);
+    public static RequestSpecification loginRequestSpecBase = with()
+            .log().uri()
+            .log().body()
+            .filter(withCustomTemplates())
+            .contentType(JSON);
 
-        public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(200)
-                .expectBody("token", notNullValue())
-                .build();
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody("token", notNullValue())
+            .build();
 
-        public static ResponseSpecification  createUserResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(201)
-                .expectBody("id", notNullValue())
-                .build();
+    public static ResponseSpecification createUserResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(201)
+            .expectBody("id", notNullValue())
+            .build();
 
-        public static ResponseSpecification  userWithEmptyFieldsResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(201)
-                .expectBody("name", is(""))
-                .expectBody("job", is(""))
-                .expectBody("id", notNullValue())
-                .build();
 
-        public static ResponseSpecification updateExistingUserResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(200)
-                .expectBody("name", notNullValue())
-                .build();
+    public static ResponseSpecification updateExistingUserResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody("name", notNullValue())
+            .build();
 
-        public static ResponseSpecification    getUserIdResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(200)
-                .expectBody("data.first_name", is("Tobias"))
-                .build();
-        public static ResponseSpecification    massiveResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(200)
-                .build();
-        public static ResponseSpecification userIdCheckResponseSpec = new ResponseSpecBuilder()
-                .log(STATUS)
-                .log(BODY)
-                .expectStatusCode(200)
-                .expectBody("data", notNullValue())
-                .build();
+    public static ResponseSpecification userIdCheckResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody("data", notNullValue())
+            .build();
 }
 
