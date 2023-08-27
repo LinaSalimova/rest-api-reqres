97f993185652ee9932d3e7de7b652128fe968514
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-08-27 20:52:57 +0300

    + changes

---
+++ src/test/java/tests/CheckAndDelateUser.java
@@ -0,0 +1,54 @@
+package tests;
+
+import models.CreateUserBody;
+import models.CreateUserResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static specs.Specs.*;
+import static specs.Specs.response204Spec;
+
+public class CheckAndDelateUser {
+
+    @Test
+    @DisplayName("Check user creation")
+    void createUserTest() {
+        String userName = "Lina";
+        String userJob = "tester";
+
+        CreateUserBody requestBody = new CreateUserBody();
+        requestBody.setName(userName);
+        requestBody.setJob(userJob);
+
+        CreateUserResponse response = step("Make a request", () -> given(requestSpec)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .spec(response201Spec)
+                .extract().as(CreateUserResponse.class));
+
+        step("Check user name in response", () ->
+                assertThat(response.getName()).isEqualTo(userName));
+
+        step("Check user ID in response", () ->
+                assertThat(response.getId()).isNotNull());
+    }
+
+    @Test
+    @DisplayName("Deleting a user")
+    void testDeleteUser() {
+        step("Make request", () ->
+                given()
+                        .spec(loginRequestSpecBase)
+                        .when()
+                        .delete("/users/2")
+                        .then()
+                        .spec(response204Spec));
+    }
+
+}
+
--- src/test/java/tests/CreatedUserDataTest.java
+++ src/test/java/tests/CreatedUserDataTest.java
@@ -1,6 +1,6 @@
 package tests;
 
-import models.CreateUserBody;
+
 import models.CreateUserResponse;
 import models.UserBody;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.Specs.*;
 
@@ -40,43 +39,6 @@ public class CreatedUserDataTest extends TestBase {
 
         step("Check job in response", () ->
                 assertEquals(userJob, response.getJob()));
-    }
-
-    @Test
-    @DisplayName("Check user creation")
-    void createUserTest() {
-        String userName = "Lina";
-        String userJob = "tester";
-
-        CreateUserBody requestBody = new CreateUserBody();
-        requestBody.setName(userName);
-        requestBody.setJob(userJob);
-
-        CreateUserResponse response = step("Make a request", () -> given(requestSpec)
-                .body(requestBody)
-                .when()
-                .post("/users")
-                .then()
-                .spec(response201Spec)
-                .extract().as(CreateUserResponse.class));
-
-        step("Check user name in response", () ->
-                assertThat(response.getName()).isEqualTo(userName));
-
-        step("Check user ID in response", () ->
-                assertThat(response.getId()).isNotNull());
-    }
-
-    @Test
-    @DisplayName("Deleting a user")
-    void testDeleteUser() {
-        step("Make request", () ->
-                given()
-                        .spec(loginRequestSpecBase)
-                        .when()
-                        .delete("/users/2")
-                        .then()
-                        .spec(response204Spec));
     }
 
 }
