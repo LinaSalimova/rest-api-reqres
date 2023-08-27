8c8182786630fa2585ff07815a98940da6d1d142
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-08-27 20:46:20 +0300

    + changes

--- src/test/java/tests/AuthApiTest.java
+++ src/test/java/tests/AuthApiTest.java
@@ -2,10 +2,10 @@ package tests;
 
 import io.qameta.allure.Owner;
 
+
 import models.Login;
 import models.LoginResponse;
-import models.UserModel;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
@@ -35,26 +35,6 @@ public class AuthApiTest extends TestBase {
                         .extract().as(LoginResponse.class));
         step("Check response", () ->
                 assertEquals(("QpwL5tke4Pnpja7X4"), response.getToken()));
-    }
-    @Test
-    @DisplayName("Edit user data")
-    void testUpdateUser() {
-        UserModel requestBody = new UserModel();
-        requestBody.setName("morpheus");
-        requestBody.setJob("zion resident");
-        UserModel response = step("Make request", () ->
-                given()
-                        .spec(loginRequestSpecBase)
-                        .body(requestBody)
-                        .when()
-                        .put("/users/2")
-                        .then()
-                        .spec(loginResponseSpec)
-                        .extract().as(UserModel.class));
-        step("Check response", () -> {
-            assertEquals("morpheus", response.getName());
-            assertEquals("zion resident", response.getJob());
-        });
     }
 
 }
